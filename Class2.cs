10bf59f18405cf15c5dca308f86dcd2324e7b946
Ennin Jeter <jeterennin@gmail.com>
2022-05-09 14:13:28 -0500

    Created Class2 file for RPSLS Project

---
+++ Class2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace RPSLS
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+            string inputPlayer, inputCPU;
+            int randomInt;
+
+            Console.WriteLine("Choose between ROCK, PAPER, SCISSORS, LIZARD and SPOCK:    ");
+            inputPlayer = Console.ReadLine();
+
+            randomInt rnd = new randomInt();
+
+            randomInt = rnd.Next(1,6);
+
+            switch (randomInt)
+            {
+                case 1:
+                    inputCPU = "ROCK";
+                    break;
+                case 2:
+                    inputCPU = "PAPER";
+                    break;
+                case 3:
+                    inputCPU = "SCISSORS";
+                    break;
+                case 4:
+                    inputCPU = "LIZARD";
+                    break;
+                case 5:
+                    inputCPU = "SPOCK";
+                    break;
+                default:
+                    break;
+            }
+
+    }
+    
+                
+                
+}
+}
